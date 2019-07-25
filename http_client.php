734f22571f3a94177eeb00091a252297f188729d
jc91715 <370234915@qq.com>
2019-07-25 17:52:29 +0800

    Create http_client.php

---
+++ http_client.php
@@ -0,0 +1,10 @@
+$fp = stream_socket_client("tcp://127.0.0.1:8080", $errno, $errstr, 30);
+if (!$fp) {
+    echo "$errstr ($errno)<br />\n";
+} else {
+    fwrite($fp, "GET / HTTP/1.0\r\nHost: www.example.com\r\nAccept: */*\r\n\r\n");
+    while (!feof($fp)) {
+        echo fgets($fp, 1024);
+    }
+    fclose($fp);
+}
